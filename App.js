fa3fd03686ba6d076f3504cd29ba68ab98ac6c6e
NathanG-HEG <92545039+NathanG-HEG@users.noreply.github.com>
2022-09-12 14:16:28 +0300

    ShoppingList

--- App.js
+++ App.js
@@ -1,20 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Pressable, StyleSheet, Text, TextInput, View, FlatList} from 'react-native';
+import {useState} from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [item, setItem] = useState("");
+    const [list, setList] = useState([]);
+
+    const addItem = () => {
+        setList([...list, item]);
+    }
+
+    const clearList = () => {
+        setList([]);
+    }
+    return (
+        <View style={styles.container}>
+            <TextInput
+                keyboardType="default"
+                style={styles.textInput}
+                onChangeText={item => setItem(item)} value={item}
+            />
+            <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+                <Pressable
+                    style={styles.button}
+                    onPress={addItem}>
+                    <Text style={styles.text}>Add</Text>
+                </Pressable>
+                <Pressable
+                    style={styles.button}
+                    onPress={clearList}>
+                    <Text style={styles.text}>Clear</Text>
+                </Pressable>
+            </View>
+            <Text style={styles.listTitle}>Shopping List</Text>
+            <View style={styles.list}>
+                <FlatList
+                    data={list}
+                    renderItem={({item}) => <Text style={styles.result}>{item}</Text>}
+                />
+            </View>
+            <StatusBar style="auto"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: 44,
+        paddingBottom: 11,
+    },
+    textInput: {
+        borderColor: "#000",
+        height: 44,
+        marginTop: 11,
+        marginBottom: 5,
+        width: 200,
+        borderWidth: 1
+    },
+    result: {
+        paddingVertical: 5,
+        fontSize: 16
+    },
+    button: {
+        paddingVertical: 11,
+        paddingHorizontal: 11,
+        marginTop: 11,
+        backgroundColor: 'lightblue',
+        marginHorizontal: 11
+    },
+    list: {
+        borderColor: "black",
+        borderWidth: 1,
+        width: 200,
+        flex: 15,
+        alignItems: 'center',
+        backgroundColor: 'white',
+        justifyContent: 'center',
+    },
+    listTitle: {
+        fontSize: 18,
+        color: "blue",
+    }
 });
